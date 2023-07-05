3e9d63a8033dd43ea38c14d12e8c2df9d8279ccf
jonnathanb2001 <buenopaul111@gmail.com>
2023-07-04 23:52:40 -0500

    modificacion en los contollers

--- src/main/java/com/tapgroup/pwsalonreservas/controller/ComplementoController.java
+++ src/main/java/com/tapgroup/pwsalonreservas/controller/ComplementoController.java
@@ -12,6 +12,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,8 +26,9 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author jonny
  */
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
-@RequestMapping("/complemento")
+@RequestMapping("/api/complemento")
 public class ComplementoController {
 
     @Autowired
